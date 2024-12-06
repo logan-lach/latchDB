0e9b9e22a0dc141c23224c39a598ba3fcb3ef04f
loganlach <lwlach@gmail.umd.edu>
2024-12-05 20:41:19 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::string input;
+    int totalInstructions = 0;
+    while (std::getline(std::cin, input))
+    {
+        ++totalInstructions;
+        std::cout << "\n User input: " << input << std::endl;
+        std::cout << "\n Total instructions: " << totalInstructions << std::endl;
+    }
+
+    return 0;
+}
